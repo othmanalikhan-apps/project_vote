b02483c26a8469c783b91118a7fe29879d04a683
OthmanEmpire <oz.alikhan@gmail.com>
2018-12-02 00:33:18 +0300

    Hero of the day

--- vote/qa/urls.py
+++ vote/qa/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     re_path(r'^$', views.splash, name='splash'),
     re_path(r'^questions$', views.questions, name='questions'),
-    re_path(r'^about$', views.about, name='about'),
+    # re_path(r'^about$', views.about, name='about'),
     re_path(r'^new_question$', views.new_question, name='new_question'),
     # re_path(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
 ]
