f4a5b93f76007b9d5e951136101f19582cf732cb
OthmanEmpire <oz.alikhan@gmail.com>
2018-12-01 16:24:52 +0300

    Implemented a simplified and working login page

--- vote/qa/urls.py
+++ vote/qa/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^$', views.index, name='index'),
-    re_path(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    re_path(r'^$', views.splash, name='splash'),
+    re_path(r'^questions$', views.index, name='questions'),
+    # re_path(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
 ]
