33ad8e381da13b95e4e36f33a428a3b5e6321be6
OthmanEmpire <oz.alikhan@gmail.com>
2018-12-03 21:58:46 +0300

    Added exportCSV function to Question model in admin page

--- vote/qa/admin.py
+++ vote/qa/admin.py
@@ -1,5 +1,27 @@
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from . import models
 
-admin.site.register(models.Question)
+
+class ExportCSV:
+    def exportCSV(self, request, querySet):
+        meta = self.model._meta
+        fieldNames = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(fieldNames)
+        for obj in querySet:
+            writer.writerow([getattr(obj, field) for field in fieldNames])
+
+        return response
+
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin, ExportCSV):
+    actions = ["exportCSV"]
